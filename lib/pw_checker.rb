401d46bdf738df4c0b036c0441ce6a1cce4e113b
Wesley Silva <wesley.silva@concretesolutions.com.br>
2014-08-02 00:37:49 -0300

    Fixing the first version (was not ok, rsrs)

--- lib/pw_checker.rb
+++ lib/pw_checker.rb
@@ -2,86 +2,68 @@ class PwChecker
 
     def initialize(password)
         @password = password
-        @points = 0
-        @methods = [:has_good_length?,
-                    :has_lower_letters?,
-                    :has_upper_letters?,
-                    :has_numbers?,
-                    :has_symbols?,
-                    :has_different_letters?,
-                    :has_different_consecutive_letters?,
-                    :is_an_unknown_word?,
-                    :has_few_vowels?]
+        @methods = self.methods.select {|m| m[/rule_/]}
+        @points = @methods.size
     end
 
-    def has_good_length?
-        @points += 1
+    def rule_has_good_length?
         return true if @password.length >= 8
         @points -= 1
         return false
     end
 
-    def has_lower_letters?
-        @points += 1
+    def rule_has_lower_letters?
         return true if @password =~ /[[:lower:]]/
         @points -= 1
         return false
     end
 
-    def has_upper_letters?
-        @points += 1
+    def rule_has_upper_letters?
         return true if @password =~ /[[:upper:]]/
         @points -= 1
         return false
     end
 
-    def has_numbers?
-        @points += 1
+    def rule_has_numbers?
         return true if @password =~ /[[:digit:]]/
         @points -= 1
         return false
     end
 
-    def has_symbols?
-        @points += 1
+    def rule_has_symbols?
         return true if @password =~ /[[:punct:]]/
         @points -= 1
         return false
     end
 
-    def has_different_letters?
-        @points += 1
-        return true if @password =~ /.*(.).*\1.*\1.*\1.*/
+    def rule_has_different_letters?
+        return true unless @password =~ /.*(.).*\1.*\1.*\1.*/
         @points -= 1
         return false
     end
 
-    def has_different_consecutive_letters?
-        @points += 1
-        return true if @password =~ /(.)\1{3}/
+    def rule_has_different_consecutive_letters?
+        return true unless @password =~ /(.)\1{3}/
         @points -= 1
         return false
     end
 
-    def is_an_unknown_word?
-        @points += 1
+    def rule_is_an_unknown_word?
         dictionary = File.read('dictionary.txt')
-        return true if dictionary.include? @password
+        return true unless dictionary.include? @password
         @points -= 1
         return false
     end
 
-    def has_few_vowels?
-        @points += 1
-        return true if @password =~ /.*[aeiou].*[aeiou].*[aeiou].*[aeiou].*/
+    def rule_has_few_vowels?
+        return true unless @password =~ /.*[aeiou].*[aeiou].*[aeiou].*[aeiou].*/
         @points -= 1
         return false
     end
 
     def classification
-        @methods.each {|m| self.send(m) }
-        return "weak" if @points < 10.0 / 3.3
-        return "medium" if @points < 10.0 / 6.6
+        return "weak" if @points < @methods.size / 3.3
+        return "medium" if @points < @methods.size / 6.6
         return "strong"
     end
 
--- spec/pwchecker_spec.rb
+++ spec/pwchecker_spec.rb
@@ -9,57 +9,57 @@ describe PwChecker do
         @password = PwChecker.new("Q1w2e3r4T5%%")
     end
 
-    describe '#has_good_length?' do
+    describe '#rule_has_good_length?' do
         it "must contain at least 8 characters" do
-            expect(@password.has_good_length?).to eq(true)
+            expect(@password.rule_has_good_length?).to eq(true)
         end
     end
 
-    describe '#has_lower_letters?' do
+    describe '#rule_has_lower_letters?' do
         it "must contain at least a lowercase letter" do
-            expect(@password.has_lower_letters?).to eq(true)
+            expect(@password.rule_has_lower_letters?).to eq(true)
         end
     end
 
-    describe '#has_upper_letters?' do
+    describe '#rule_has_upper_letters?' do
         it "must contain at least a uppercase letter" do
-            expect(@password.has_upper_letters?).to eq(true)
+            expect(@password.rule_has_upper_letters?).to eq(true)
         end
     end
 
-    describe '#has_numbers?' do
+    describe '#rule_has_numbers?' do
         it "must contain at least a number" do
-            expect(@password.has_numbers?).to eq(true)
+            expect(@password.rule_has_numbers?).to eq(true)
         end
     end
 
-    describe '#has_symbols?' do
+    describe '#rule_has_symbols?' do
         it "must contain at least a symbol" do
-            expect(@password.has_symbols?).to eq(true)
+            expect(@password.rule_has_symbols?).to eq(true)
         end
     end
 
-    describe '#has_different_letters?' do
+    describe '#rule_has_different_letters?' do
         it "must not repeat 4 times the same character" do
-            expect(@password.has_different_letters?).not_to eq(true)
+            expect(@password.rule_has_different_letters?).to eq(true)
         end
     end
 
-    describe '#has_different_consecutive_letters?' do
+    describe '#rule_has_different_consecutive_letters?' do
         it "must not repeat consecutively 4 times the same character" do
-            expect(@password.has_different_consecutive_letters?).not_to eq(true)
+            expect(@password.rule_has_different_consecutive_letters?).to eq(true)
         end
     end
 
-    describe '#is_an_unknown_word?' do
+    describe '#rule_is_an_unknown_word?' do
         it "must not be a dictonary word" do
-            expect(@password.is_an_unknown_word?).not_to eq(true)
+            expect(@password.rule_is_an_unknown_word?).to eq(true)
         end
     end
 
-    describe '#has_few_vowels?' do
+    describe '#rule_has_few_vowels?' do
         it "must not have more than 4 vowels" do
-            expect(@password.has_few_vowels?).not_to eq(true)
+            expect(@password.rule_has_few_vowels?).to eq(true)
         end
     end
 
